f9a0b68b8e123df9dd6cd6940e21aae31175054f
Wang Ran <rw6e14@soton.ac.uk>
2015-04-29 22:50:47 +0100

    sorting

--- jobmatch/public/js/lmi.js
+++ jobmatch/public/js/lmi.js
@@ -1,109 +1,79 @@
-// API Demo JavaScript
-/*
-var getInfo = function(soc) {
-    $.get("http://api.lmiforall.org.uk/api/v1/soc/code/" + soc, function(data) {
-        $("#info-box h3").text(data.title);
-        $("#info-box p.first").text(data.description);
-        $("#info-box p.second").text(data.tasks);
-    });
-    $.get("http://api.lmiforall.org.uk/api/v1/ashe/estimatePay",
-    { soc: soc, coarse: "false", breakdown: "gender"},
-    function(data) {
-        $("#graph-box").html("<p><b>Male:</b> " + data.series[0].breakdown[1].estpay + " GBP/week </p>" +
-            "<p><b>Female:</b> " + data.series[0].breakdown[0].estpay + " GBP/week </p>");
-    });
-};
-
-$(function() {
-    $("#search-go").click(function() {
-        $.get("http://api.lmiforall.org.uk/api/v1/soc/search", { q: $("#search-input").val() }, function(data, status) {
-            $("#list table tbody").empty();
-            $.each(data, function(i, e) {
-                var tr = $("<tr><td>" + e.soc + "</td><td>" + e.title + "</td></tr>");
-                (function(tr, soc) {
-                    tr.click(function() {
-                        getInfo(soc);
-                    });
-                })(tr, e.soc);
-                $("#list table tbody").append(tr);
-            });
-        }, "json");
-    });
-});*/
-
 // modulematch, find keywords
 $(function() {
     $("#search-vacancies").click(function() {
-        $.get("http://api.lmiforall.org.uk/api/v1/vacancies/search", { keywords: $("#search-input").val() }, function(data, status) {
+        $.get("http://api.lmiforall.org.uk/api/v1/vacancies/search", {
+            keywords: $("#search-input").val()
+        }, function(data, status) {
             $("#list table tbody").empty();
             $.each(data, function(i, e) {
                 var tr = $("<tr><td>" + e.id + "</td><td>" + e.title + "</td></tr>");
                 (function(tr, vacancies) {
                     tr.click(function() {
-                        var description=(e.summary).replace(/\//g," "); //escape slash in parameter
+                        var description = (e.summary).replace(/\//g, " "); //escape slash in parameter
                         $("#info-box p.first").text(e.summary);
                         /*$.get("http://jobsearch.direct.gov.uk/GetJob.aspx?JobID="+e.id,function(data,status){
                             alert("hello");
-                        });*/$("#courses table tbody tr td").remove();
-                        $.get("/example/"+encodeURIComponent(description),function(data){
+                        });*/
+                        $("#courses table tbody tr td").remove();
+                        $.get("/example/" + encodeURIComponent(description), function(data) {
                             //var count=0;
                             $("#keywords table tbody").empty();
-                            $.each(data,function(i, e){
+                            $.each(data, function(i, e) {
                                 /*if(data[i]._typeGroup=="topics"){
                                     alert(data[i].categoryName);
                                 }else {
                                     alert(data[i].name);
                                 }*/
                                 
+                                    if (data[i]._typeGroup == "topics") {
+                                        var k = $("<tr><td>" + data[i]._typeGroup + "</td><td></td><td>" + data[i].categoryName + "</td></tr>");
+                                            $.get("/database/" + encodeURIComponent(data[i].categoryName), function(data) {
+                                                if (data.length > 0) {
+                                                    $("#courses table tbody tr td").remove();
+                                                }
+                                                $.each(data, function(i, e) {
+                                                    //count++;
+                                                    var m = $("<tr><td>" + (i + 1) + "</td><td>" + data[i].courseNo + "</td><td>" + data[i].courseName + "</td><td>" + data[i].point + "</td></tr>");
+                                                    /*if(count<13){*/
+                                                    $("#courses table tbody").append(m); //}
+                                                });
 
-                                    if(data[i]._typeGroup=="topics"){
-                                        var k=$("<tr><td>"+data[i]._typeGroup+"</td><td></td><td>" +data[i].categoryName+"</td></tr>");
+                                            });
+                                        //each keywords search course
+
+                                    } else if (data[i]._typeGroup == "socialTag") {
+                                        var k = $("<tr><td>" + data[i]._typeGroup + "</td><td></td><td>" + data[i].name + "</td></tr>");
 
                                         //each keywords search course
-                                        $.get("/database/"+encodeURIComponent(data[i].categoryName),function(data){
-                                            if(data.length>0){
+                                        $.get("/database/" + encodeURIComponent(data[i].name), function(data) {
+                                            if (data.length > 0) {
                                                 $("#courses table tbody tr td").remove();
                                             }
-                                            $.each(data,function(i, e){
+                                            $.each(data, function(i, e) {
                                                 //count++;
-                                                var m=$("<tr><td>"+(i+1)+"</td><td>"+data[i].courseNo+"</td><td>"+data[i].courseName+"</td><td>" +data[i].point+"</td></tr>");
-                                                /*if(count<13){*/$("#courses table tbody").append(m);//}
+                                                var m = $("<tr><td>" + (i + 1) + "</td><td>" + data[i].courseNo + "</td><td>" + data[i].courseName + "</td><td>" + data[i].point + "</td></tr>");
+                                                /*if(count<13){*/
+                                                $("#courses table tbody").append(m); //}
+                                            });
+                                        });
+                                    } else if (data[i]._typeGroup == "entities") {
+                                        var k = $("<tr><td>" + data[i]._typeGroup + "</td><td>" + data[i]._type + "</td><td>" + data[i].name + "</td></tr>");
+
+                                        //each keywords search course
+                                        $.get("/database/" + encodeURIComponent(data[i].name), function(data) {
+                                            if (data.length > 0) {
+                                                $("#courses table tbody tr td").remove();
+                                            }
+                                            $.each(data, function(i, e) {
+                                                //count++;
+                                                var m = $("<tr><td>" + (i + 1) + "</td><td>" + data[i].courseNo + "</td><td>" + data[i].courseName + "</td><td>" + data[i].point + "</td></tr>")
+                                                    /*if(count<13){*/
+                                                    $("#courses table tbody").append(m); //}
                                             });
 
                                         });
-                                    }else if (data[i]._typeGroup=="socialTag"){
-                                        var k=$("<tr><td>"+data[i]._typeGroup+"</td><td></td><td>" +data[i].name+"</td></tr>");
-
-                                        //each keywords search course
-                                        $.get("/database/"+encodeURIComponent(data[i].name),function(data){
-                                            if(data.length>0){
-                                                $("#courses table tbody tr td").remove();
-                                            }
-                                            $.each(data,function(i, e){
-                                                //count++;
-                                                var m=$("<tr><td>"+(i+1)+"</td><td>"+data[i].courseNo+"</td><td>"+data[i].courseName+"</td><td>" +data[i].point+"</td></tr>");
-                                                /*if(count<13){*/$("#courses table tbody").append(m);//}
-                                            });
-
-                                        });
-                                    }else if (data[i]._typeGroup=="entities"){
-                                        var k=$("<tr><td>"+data[i]._typeGroup+"</td><td>"+data[i]._type+"</td><td>" +data[i].name+"</td></tr>");
-
-                                        //each keywords search course
-                                        $.get("/database/"+encodeURIComponent(data[i].name),function(data){
-                                            if(data.length>0){
-                                                $("#courses table tbody tr td").remove();
-                                            }
-                                            $.each(data,function(i, e){
-                                                //count++;
-                                                var m=$("<tr><td>"+(i+1)+"</td><td>"+data[i].courseNo+"</td><td>"+data[i].courseName+"</td><td>" +data[i].point+"</td></tr>")
-                                                /*if(count<13){*/$("#courses table tbody").append(m);//}
-                                            });
-
-                                        });
-                                    }
-                                    $("#keywords table tbody").append(k);
-
+                                    }                               
+                                $("#keywords table tbody").append(k);
                             });
                         });
                     });
--- jobmatch/routes/database.js
+++ jobmatch/routes/database.js
@@ -15,65 +15,80 @@ exports.db = function(req, res) {
     var count = [];
     var course_output = [];
     var arr = [];
-
-    db.serialize(function() {
         if(sess.match == 1){ //previous matched course
-              course_output = sess.course_output;
-              count = sess.count;               
-              console.log("session start:"+course_output);
-              //console.log("session start:"+sess.match);
-        } 
+            course_output = sess.course_output;
+            count = sess.count;               
+            //console.log("*****"+keyword+"session start:"+course_output);
+            //console.log("session start:"+sess.match);
+      }else if(sess.match == 0){ /*console.log("*****"+keyword+"session start0:"+course_output);*/ };
+      
+          db.serialize(function() {
+              if(keyword!=null||keyword!=undefined){
+                    sess.match = 1;
+                    res.session = sess;
+                    db.each("SELECT course FROM keywordID WHERE keyword LIKE '%" + keyword + "%'", function(err, rows) {
+                    if (rows == null) {
+                        res.send("No Match keyword!");
+                    }
+                    var exist = -1;
+                    for (var i = 0; i < course_output.length; i++) {
+                        if (rows.course == course_output[i] && rows.course != undefined && exist == -1) { //exist in array
+                            exist = i;
+                        }
+                    }
+                    if (exist == -1 && rows.course != undefined) { //new added
+                        course_output.push(rows.course);
+                        count[course_output.length - 1] = 1;
+                    } else if (exist != -1) { // already exist
+                        count[exist]++;
+                        exist == -1;
+                    }
+                }, function() {
+                    sess.course_output = course_output;
+                    sess.count = count;
+                    
+                    //console.log("*****"+keyword+"session end1:"+course_output);
+                    //console.log("session end:"+sess.match);
+                    res.session = sess;
+                    for (var i = 0; i < course_output.length; i++) {
+                        var param = {
+                            'courseNo': course_output[i],
+                            'courseName': jsonQuery("courseNo[id=" + course_output[i] + "].name", {
+                                data: data
+                            }).value,
+                            'point': count[i]
+                        }
+                         // console.log(course_output[i] + "," + jsonQuery("courseNo[id=" + course_output[i] + "].name", {
+                         //     data: data
+                         // }).value + "," + count[i]);
+                        arr.push(param);
+                    }
+                    //console.log(res.session);
+                    arr.sort(sortByPoint);
+                    res.send(arr);
 
-        if(keyword!=null||keyword!=undefined){
-              db.each("SELECT course FROM keywordID WHERE keyword LIKE '%" + keyword + "%'", function(err, rows) {
-              if (rows == null) {
-                  res.send("No Match keyword!");
+                });
+              }else{
+                  sess.course_output = course_output;
+                  sess.count = count;
+                  sess.match = 1;
+                  //console.log("*****"+keyword+"session end2:"+course_output);
+                  //console.log("session end:"+sess.match);
+                  res.session = sess;
+                  res.send(arr);
               }
-              var exist = -1;
-              for (var i = 0; i < course_output.length; i++) {
-                  if (rows.course == course_output[i] && rows.course != undefined && exist == -1) { //exist in array
-                      exist = i;
-                  }
-              }
-              if (exist == -1 && rows.course != undefined) { //new added
-                  course_output.push(rows.course);
-                  count[course_output.length - 1] = 1;
-              } else if (exist != -1) { // already exist
-                  count[exist]++;
-                  exist == -1;
-              }
-          }, function() {
-              sess.course_output = course_output;
-              sess.count = count;
-              sess.match = 1;
-              console.log("session end:"+course_output);
-              //console.log("session end:"+sess.match);
-              res.session = sess;
-              for (var i = 0; i < course_output.length; i++) {
-                  var param = {
-                      'courseNo': course_output[i],
-                      'courseName': jsonQuery("courseNo[id=" + course_output[i] + "].name", {
-                          data: data
-                      }).value,
-                      'point': count[i]
-                  }
-                   // console.log(course_output[i] + "," + jsonQuery("courseNo[id=" + course_output[i] + "].name", {
-                   //     data: data
-                   // }).value + "," + count[i]);
-                  arr.push(param);
-              }
-              res.send(arr);
           });
-        }else{
-            sess.course_output = course_output;
-            sess.count = count;
-            sess.match = 1;
-            console.log("session end:"+course_output);
-            //console.log("session end:"+sess.match);
-            res.session = sess;
-            res.send(arr);
-        }
-    });
+    
     db.close();
+}
 
+function sortByPoint(a, b) {
+    var sortStatus = 0;
+ 
+    if (a.point > b.point) {
+        sortStatus = -1;
+    } else if (a.point < b.point) {
+            sortStatus = 1;
+    }
+    return sortStatus;
 }
